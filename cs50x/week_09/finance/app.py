289f98e75c36355d843c4c13b1b8a700af433f96
Maciek <109960378+MaciejMagic@users.noreply.github.com>
2022-12-19 17:57:28 +0100

    Update app.py

--- cs50x/week_09/finance/app.py
+++ cs50x/week_09/finance/app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
@@ -44,12 +45,25 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
 
-    """Odds are youll want to execute multiple SELECTs. Depending on how you implement your table(s),
-    you might find GROUP BY HAVING SUM and/or WHERE of interest."""
+    # initialize list of user current stocks (dicts)
+    currentStocks = []
 
-    """Odds are youll want to call lookup for each stock."""
+    # extract total of user shares for each stock from db (list of dicts)
+    portfolio = db.execute("SELECT symbol, SUM(share) FROM shares WHERE user_id = ? GROUP BY symbol", session["user_id"])
 
-    return apology("TODO")
+    # populate the currentStocks list with user stocks (with latest prices)
+    for stock in range(len(portfolio)):
+
+        # for each company (stock) lookup() creates a dict with keys: "name", "price", "symbol"
+        currentStocks.append(lookup(portfolio[stock]["symbol"]))
+
+        # adding "shares" key to those dicts
+        currentStocks[stock]["shares"] = portfolio[stock]["share"]
+
+        # fetch current user cash
+        cash = db.execute("SELECT cash FROM users WHERE user_id = ?", session["user_id"])
+
+    return render_template("index.html", currentStocks, cash)
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -63,14 +77,14 @@ def buy():
 
         # Render an apology if the input is blank or the symbol does not exist
         if not buySymbol or not buyStock:
-            return apology("must provide a valid stock symbol")
+            return apology("must provide a valid stock symbol", 403)
 
         # Require that a user input a number of shares, implemented as a field whose name is shares
         nrOfShares = int(request.form.get("shares"))
 
         # Render an apology if the input is not a positive integer
         if nrOfShares < 0:
-            return apology("must provide a positive number")
+            return apology("must provide a positive number", 403)
 
         stockPrice = buyStock["price"]
         user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
@@ -79,7 +93,7 @@ def buy():
 
         # Render an apology, without completing a purchase, if the user cannot afford the number of shares at the current price
         if sharesToBuyTotal < userCash:
-            return apology("you do not have funds for this purchase")
+            return apology("you do not have funds for this purchase", 403)
 
         # Substract cash spent from user account
         db.execute("UPDATE users SET cash = ? WHERE user_id = ?", userCash - sharesToBuyTotal, session["user_id"])
@@ -87,23 +101,25 @@ def buy():
         # Add one or more new tables to finance.db via which to keep track of the purchase
         updateSharesTable = db.execute("""INSERT INTO shares
                                        (user_id, symbol, share, price, time)
-                                       VALUES (?, ?, ?, ?, ?)""", session["user_id"], buySymbol, nrOfShares, stockPrice, ??? )
+                                       VALUES (?, ?, ?, ?, ?)""", session["user_id"], buySymbol, nrOfShares, stockPrice, datetime.datetime.now())
 
         if not updateSharesTable:
-            # Define UNIQUE indexes on any fields that should be unique
-            # Define (non-UNIQUE) indexes on any fields via which you will search (as via SELECT with WHERE)
+            # Define UNIQUE indexes on any fields that should be unique / TO DO
+            # Define (non-UNIQUE) indexes on any fields via which you will search (as via SELECT with WHERE) / TO DO
             db.execute("""
                 CREATE TABLE shares (
-                    id INTEGER PRIMARY KEY NOT NULL,
+                    id INTEGER NOT NULL,
                     user_id INTEGER NOT NULL,
                     symbol TEXT NOT NULL,
                     share INTEGER NOT NULL,
                     price REAL NOT NULL,
-                    time TIMESTAMP NOT NULL
+                    time TIMESTAMP NOT NULL,
+                    PRIMARY KEY(id),
+                    FOREIGN KEY(user_id) REFERENCES users(id)
                     )""")
             db.execute("""INSERT INTO shares
                        (user_id, symbol, share, price, time)
-                       VALUES (?, ?, ?, ?, ?)""", session["user_id"], buySymbol, nrOfShares, stockPrice, ??? )
+                       VALUES (?, ?, ?, ?, ?)""", session["user_id"], buySymbol, nrOfShares, stockPrice, datetime.datetime.now())
 
         # Upon completion, redirect the user to the home page
         return redirect("index.html")
@@ -116,13 +132,10 @@ def buy():
 def history():
     """Show history of transactions"""
 
-    """For each row, make clear whether a stock was bought or sold and include the stocks symbol,
-    the (purchase or sale) price, the number of shares bought or sold,
-    and the date and time at which the transaction occurred."""
+    # Query db for list of user transactions
+    history = db.execute("SELECT symbol, share, price, time FROM shares WHERE user_id = ? ORDER BY time DESC", session["user_id"])
 
-    """You might need to alter the table you created for buy or supplement it with an additional table. Try to minimize redundancies."""
-
-    return apology("TODO")
+    return render_template("history.html", history)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -182,15 +195,15 @@ def quote():
         symbol = request.form.get("symbol")
 
         if not symbol:
-            return apology("must provide stock symbol")
+            return apology("must provide stock symbol", 403)
 
         # Require that the symbol provided is valid
         quoteResponse = lookup(symbol)
 
         if not quoteResponse:
-            return apology("must provide a valid stock symbol")
+            return apology("must provide a valid stock symbol", 403)
 
-        return render_template("quoted.html", name=quoteResponse["name"], symbol=quoteResponse["symbol"], price=quoteResponse["price"])
+        return render_template("quoted.html", name=quoteResponse['name'], symbol=quoteResponse['symbol'], price=quoteResponse['price'])
 
     return render_template("quote.html")
 
@@ -200,47 +213,47 @@ def register():
     """Register user"""
     if request.method == "POST":
 
-        # Require that a user inputs a username, implemented as a text field whose name is username
+        # Require that a user inputs a username
         username = request.form.get("username")
 
         # Render an apology if the user username input is blank
         if not username:
-            return apology("must provide a username")
+            return apology("must provide a username", 403)
 
         nameInDb = db.execute("SELECT * FROM users WHERE username = ?", username)
 
         # Render an apology if the user username input already exists
         if nameInDb:
-            return apology("that username is already taken")
+            return apology("that username is already taken", 403)
 
-        # Require that a user input a password, implemented as a text field whose name is password...
+        # Require that a user input a password
         password = request.form.get("password")
 
         # Render an apology if the user password input is blank
         if not password:
-            return apology("must provide a password")
+            return apology("must provide a password", 403)
 
-        # ...and then that same password again, implemented as a text field whose name is confirmation
+        # ...and then that same password again
         confirmation = request.form.get("confirmation")
 
         # Render an apology if the user password confirmation input is blank
         if not confirmation:
-            return apology("must provide a matching password in both boxes")
+            return apology("must provide a matching password in both boxes", 403)
 
         # Render an apology if the user password and confirmation input do not match
         if password != confirmation:
-            return apology("passwords do not match")
+            return apology("passwords do not match", 403)
 
         # Hash the users password with generate_password_hash
         passwordHash = generate_password_hash(password)
 
-        # Insert the new user into users, storing a hash of the users password, not the password itself
+        # Add the new user into users, storing a hash of the users password, not the password itself
         db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, passwordHash)
 
         userId = db.execute("SELECT id FROM users WHERE username = ?", username)
 
         # log user in
-        session["user_id"] = userId
+        session["user_id"] = userId[0]["id"]
 
         return render_template("index.html")
 
@@ -251,17 +264,59 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    
-    """Require that a user input a stocks symbol, implemented as a select menu whose name is symbol.
-    Render an apology if the user fails to select a stock or if (somehow, once submitted)
-    the user does not own any shares of that stock."""
 
-    """Require that a user input a number of shares, implemented as a field whose name is shares.
-    Render an apology if the input is not a positive integer or if the user does not own that many shares of the stock."""
+    if request.method == "POST":
 
-    """Submit the users input via POST to /sell."""
+        # Require that a user input a stocks symbol
+        sellSymbol = request.form.get("symbol")
+        sellStock = lookup(sellSymbol)
 
-    """Upon completion, redirect the user to the home page."""
+        # Render an apology if user didnt select any stock from the list
+        if not sellSymbol or sellSymbol == "Symbol":
+            return apology("must choose a stock owned from the dropdown list", 403)
 
+        # retrieve user info of shares of the stock provided
+        userShares = db.execute("SELECT SUM(share) FROM shares WHERE id = ? AND symbol = ? GROUP BY share", session["user_id"], sellSymbol)
 
-    return apology("TODO")
+        userSharesNr = int(userShares[0]["share"])
+
+        # Render an apology if user does not have any shares of the provided stock
+        if userSharesNr < 1:
+            return apology("you do not own any shares of that stock", 403)
+
+        sellUserShares = int(request.form.get("shares"))
+
+        # Render an apology if the input is not a positive integer
+        if sellUserShares < 1:
+            return apology("must provide a positive number", 403)
+
+        # Render an apology if user does not own that many shares
+        if sellUserShares > userSharesNr:
+            return apology("must provide a number less than or equal to the shares that you own", 403)
+
+        # Fetch latest stock price
+        currentStockPrice = sellStock["price"]
+
+        # Fetch current user account cash balance
+        user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
+        userCash = user[0]["cash"]
+
+        # Calculate amount of cash for sold shares
+        profit = currentStockPrice * sellUserShares
+
+        soldUserShares = sellUserShares - 2 * sellUserShares
+
+        # Substract shares from user portfolio (add entry with negative shares amount to shares table)
+        db.execute("""INSERT INTO shares
+                      (user_id, symbol, share, price, time)
+                      VALUES (?, ?, ?, ?, ?)""", session["user_id"], sellSymbol, soldUserShares, currentStockPrice, datetime.datetime.now())
+
+        # Add cash from sale to user account
+        db.execute("UPDATE users SET cash = ? WHERE user_id = ?", userCash + profit, session["user_id"])
+
+        return redirect("index.html")
+
+    # Retrieve a [list] of user shares of [dicts] stocks
+    userStocks = db.execute("SELECT symbol, SUM(share) FROM shares WHERE user_id = ? GROUP BY symbol", session["user_id"])
+
+    return render_template("sell.html", userStocks)
